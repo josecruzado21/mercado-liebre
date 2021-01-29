1818f9712675d2eb9dcbac1e7e4cf2c7a3512fab
josecruzado21 <josecruzado2103@gmail.com>
2021-01-28 21:41:14 -0500

    mercado liebre

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const path=require('path');
+app.use('/elements',express.static(__dirname+'/public'))
+app.listen(3000,()=>console.log('Servidor operando correctamente'));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/ingresa.html'))
+})
