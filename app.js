baa18a6872969b3d01412358738019c9212f0ba1
josecruzado21 <josecruzado2103@gmail.com>
2021-01-28 21:48:34 -0500

    mercado liebre 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const path=require('path');
 app.use('/elements',express.static(__dirname+'/public'))
-app.listen(3000,()=>console.log('Servidor operando correctamente'));
+app.listen(process.env.PORT||3000,()=>console.log('Servidor operando correctamente'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
